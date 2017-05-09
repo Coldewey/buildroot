77d8672bd5c52b008db9cf60a2c8226e4ee47eb4
Phil Lehmkuhl <plehmkuhl@coldewey.de>
2017-05-09 17:35:17 +0200

    Changes to gconf according to https://patchwork.ozlabs.org/patch/181873/

--- package/gconf/gconf.mk
+++ package/gconf/gconf.mk
@@ -4,21 +4,21 @@
 #
 #############################################################
 
-GCONF_VERSION = 3.2.0
+GCONF_MAJOR_VERSION = 3.2
+GCONF_MINOR_VERSION = 0
+GCONF_VERSION = $(GCONF_MAJOR_VERSION).$(GCONF_MINOR_VERSION)
 GCONF_SOURCE = GConf-$(GCONF_VERSION).tar.xz
 GCONF_SITE = http://ftp.gnome.org/pub/gnome/sources/GConf/3.2
-GCONF_DEPENDENCIES = dbus-glib libxml2
+GCONF_DEPENDENCIES = dbus-glib host-intltool libxml2
 GCONF_INSTALL_STAGING = YES
 GCONF_CONF_OPTS += --sysconfdir=/etc --libexecdir=/usr/lib/GConf --disable-orbit
 GCONF_LICENSE = GPLv2
 GCONG_LICENSE_FILE = COPYING
 
-define GCONF_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
-
-	(cd $(@D); \
-	install -v -m755 -d $(TARGET_DIR)/etc/gconf/gconf.xml.system)
-
+define GCONF_POST_INSTALL
+	$(INSTALL) -v -m755 -d $(TARGET_DIR)/etc/gconf/gconf.xml.system)
 endef
+
+GCONF_POST_INSTALL_TARGET_HOOKS += GCONF_POST_INSTALL
 
 $(eval $(autotools-package))
